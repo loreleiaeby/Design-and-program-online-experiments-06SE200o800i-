df0dc7b5698cc749cb90754c268c086f538a8c32
lorelei_aeby <lorelei.aeby@gmail.com>
2026-04-12 17:34:41 +0200

    Update session5.js

--- Session5/laeby/session5.js
+++ Session5/laeby/session5.js
@@ -1,12 +1,11 @@
-// let ageGroup;
-// const age = 25;
-// if(age<18){
-//     ageGroup = "Child";
-
-// }else{
-//     ageGroup="Adult";
-// }
-// console.log("ageGroup:", ageGroup);
+let ageGroup;
+const age = 25;
+if(age<18){
+   ageGroup = "Child";
+}else{
+    ageGroup="Adult";
+}
+console.log("ageGroup:", ageGroup);
 
 let age = 18;
 let ageGroup = age<18?"child":"adult";
@@ -14,11 +13,4 @@ console.log("ageGroup:", ageGroup);
 
 for(let i=0; i < 5; i++){
     console.log("i/number:", i+1);
-}
-
-let ageGroup;
-if (age < 18) {
-    ageGroup = 'Child';
-} else {
-    ageGroup = 'Adult'
 }
